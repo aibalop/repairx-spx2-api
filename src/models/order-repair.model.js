c83e18f49f7563b1d9c339b9e9d3b20c913c99ff
Alan Ibarra <alanibarralop@gmail.com>
2022-11-19 00:43:24 -0700

    Feat: little changes in order repair schema

--- src/models/order-repair.model.js
+++ src/models/order-repair.model.js
@@ -3,48 +3,132 @@ import softDelete from 'mongoose-delete';
 import paginate from 'mongoose-paginate-v2';
 import currency from 'currency.js';
 
-const orderRepairSchema = new mongoose.Schema({
-    orderId: { type: String, required: [true, 'ORDER ID es requerido'], unique: [true, 'El OrderID ya existe'], index: true },
-    customer: {
-        customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: [true, 'Cliente es requerido'] },
-        name: { type: String },
-        lastName: { type: String },
-        surName: { type: String },
-        phone: { type: String },
-        email: { type: String },
+const orderRepairSchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: String,
+      required: [true, 'ORDER ID es requerido'],
+      unique: [true, 'El OrderID ya existe'],
+      index: true,
     },
-    works: [{
-        workId: { type: mongoose.Schema.Types.ObjectId, ref: 'Work', required: [true, 'ID de servicio es requerido'] },
-        name: { type: String, required: [true, 'Nombre de servicio es requerido'] },
-        amount: { type: Number, required: [true, 'Monto de servicio es requerido'] },
-        notes: { type: String }
-    }],
-    charges: [{
-        chargeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Charge', required: [true, 'ID de cargo es requerido'] },
-        name: { type: String, required: [true, 'Nombre de cargo es requerido'] },
-        amount: { type: Number, required: [true, 'Monto de cargo es requerido'] },
-        notes: { type: String }
-    }],
-    devices: [{
-        deviceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Device', required: [true, 'ID de dispositivo es requerido'] },
-        name: { type: String, required: [true, 'El nombre de dispositivo es requerido'], uppercase: true, trim: true },
+    customer: {
+      customerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Customer',
+        required: [true, 'Cliente es requerido'],
+      },
+      name: { type: String },
+      lastName: { type: String },
+      surName: { type: String },
+      phone: { type: String },
+      email: { type: String },
+    },
+    works: [
+      {
+        workId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Work',
+          required: [true, 'ID de servicio es requerido'],
+        },
+        name: {
+          type: String,
+          required: [true, 'Nombre de servicio es requerido'],
+        },
+        amount: {
+          type: Number,
+          required: [true, 'Monto de servicio es requerido'],
+        },
+        notes: { type: String },
+      },
+    ],
+    charges: [
+      {
+        chargeId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Charge',
+          required: [true, 'ID de cargo es requerido'],
+        },
+        name: {
+          type: String,
+          required: [true, 'Nombre de cargo es requerido'],
+        },
+        amount: {
+          type: Number,
+          required: [true, 'Monto de cargo es requerido'],
+        },
+        notes: { type: String },
+      },
+    ],
+    devices: [
+      {
+        device: {
+          deviceId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Device',
+            required: [true, 'ID de dispositivo es requerido'],
+          },
+          name: {
+            type: String,
+            required: [true, 'El nombre de dispositivo es requerido'],
+            uppercase: true,
+            trim: true,
+          },
+        },
         brand: {
-            brandId: { type: mongoose.Schema.Types.ObjectId, ref: 'Brand', required: [true, 'ID de la marca es requerido'] },
-            name: { type: String, required: [true, 'La marca es requerida'], uppercase: true, trim: true },
+          brandId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Brand',
+            required: [true, 'ID de la marca es requerido'],
+          },
+          name: {
+            type: String,
+            required: [true, 'La marca es requerida'],
+            uppercase: true,
+            trim: true,
+          },
         },
         model: { type: String, uppercase: true, trim: true },
-        accessory: { type: String, required: [true, 'Capture el accesorio'], uppercase: true, trim: true },
+        accessory: {
+          type: String,
+          required: [true, 'Capture el accesorio'],
+          uppercase: true,
+          trim: true,
+        },
         itsOn: { type: Boolean, default: false },
-        cards: { type: String, enum: ['SIM', 'SD', 'SIM/SD'], uppercase: true, trim: true },
+        cards: {
+          type: String,
+          enum: ['SIM', 'SD', 'SIM/SD'],
+          uppercase: true,
+          trim: true,
+        },
         password: { type: String },
-        details: { type: String, required: [true, 'Capture detalles del dispositivo'], uppercase: true, trim: true },
-        customerReport: { type: String, required: [true, 'Capture reporte del cliente'], uppercase: true, trim: true },
+        details: {
+          type: String,
+          required: [true, 'Capture detalles del dispositivo'],
+          uppercase: true,
+          trim: true,
+        },
+        customerReport: {
+          type: String,
+          required: [true, 'Capture reporte del cliente'],
+          uppercase: true,
+          trim: true,
+        },
         finalDiagnosis: { type: String, uppercase: true, trim: true },
         warrantyDays: { type: Number, default: 0 },
-        status: { type: String, enum: ['Pendiente', 'En Progreso', 'Terminado'], default: 'Pendiente' },
-    }],
+        status: {
+          type: String,
+          enum: ['Pendiente', 'En Progreso', 'Terminado'],
+          default: 'Pendiente',
+        },
+      },
+    ],
     deliveryDate: { type: Date },
-    status: { type: String, enum: ['Pendiente', 'Completada'], default: 'Pendiente' },
+    status: {
+      type: String,
+      enum: ['Pendiente', 'Completada'],
+      default: 'Pendiente',
+    },
     isPaid: { type: Boolean, default: false },
     remainingAmount: { type: Number, required: true },
     discountAmount: { type: Number, default: 0 },
@@ -52,26 +136,34 @@ const orderRepairSchema = new mongoose.Schema({
     subtotalAmount: { type: Number, required: true },
     totalAmount: { type: Number, required: true },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-}, {
+    updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  },
+  {
     timestamps: true,
-    collection: 'order-repairs'
-});
+    collection: 'order-repairs',
+  }
+);
 
-orderRepairSchema.plugin(softDelete, { deletedAt: true, deletedBy: true, overrideMethods: true });
+orderRepairSchema.plugin(softDelete, {
+  deletedAt: true,
+  deletedBy: true,
+  overrideMethods: true,
+});
 orderRepairSchema.plugin(paginate);
 
 orderRepairSchema.pre('validate', async function (next) {
-    const totalAmountCurrency = currency(this.get('totalAmount'));
-    const remainingAmount = totalAmountCurrency.subtract(this.get('advanceAmount')).value;
-    const isPaid = remainingAmount <= 0;
-    this.set('remainingAmount', isPaid ? 0 : remainingAmount);
-    this.set('isPaid', isPaid);
+  const totalAmountCurrency = currency(this.get('totalAmount'));
+  const remainingAmount = totalAmountCurrency.subtract(
+    this.get('advanceAmount')
+  ).value;
+  const isPaid = remainingAmount <= 0;
+  this.set('remainingAmount', isPaid ? 0 : remainingAmount);
+  this.set('isPaid', isPaid);
 
-    const count = await OrderRepair.countDocumentsWithDeleted();
-    this.set('orderId', `${count + 1}`.padStart(4, '0'));
+  const count = await OrderRepair.countDocumentsWithDeleted();
+  this.set('orderId', `${count + 1}`.padStart(4, '0'));
 
-    next();
+  next();
 });
 
 const OrderRepair = mongoose.model('OrderRepair', orderRepairSchema);
